e4bba1055cc04dd56337aba35f43b6b542b510c0
Hayao Sato <hayaos@jp.ibm.com>
2018-01-19 16:58:13 +0900

    Enable vuetify

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+<v-app id="inspire" dark>
+  <v-navigation-drawer clipped fixed v-model="drawer" app>
+    <side-bar></side-bar>
+  </v-navigation-drawer>
+  <v-toolbar app fixed clipped-left>
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar-title>Watson Conversation Dialog Tester</v-toolbar-title>
+  </v-toolbar>
+  <v-content>
+    <v-container fluid fill-height>
+      <router-view></router-view>
+    </v-container>
+  </v-content>
+  <v-footer app fixed>
+    <span>&copy; 2017</span>
+  </v-footer>
+</v-app>
 </template>
 
 <script>
-  export default {
-    name: 'dialog-tester-app'
+import appHeader from '@/components/AppHeader'
+import sideBar from '@/components/SideBar'
+
+export default {
+  name: 'dialog-tester-app',
+  components: {
+    'app-header': appHeader,
+    'side-bar': sideBar
+  },
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
   }
+}
 </script>
 
 <style>
-  /* CSS */
+/* CSS */
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 
 import App from './App'
 import router from './router'
 import store from './store'
+
+import('vuetify/dist/vuetify.min.css')
+
+Vue.use(Vuetify)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
