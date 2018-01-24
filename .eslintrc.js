d1c2d20f53fda24dc5e220df7adc8f3c85411081
Hayao Sato <hayaos@jp.ibm.com>
2018-01-24 18:13:15 +0900

    Update for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
+    // rule for space before paren
+    "space-before-function-paren": ["error", {"anonymous": "always", "named": "never", "asyncArrow": "always"}],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
