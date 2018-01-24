79b76038b0d600acb399664f7cb81d40b34b68b0
Hayao Sato <hayaos@jp.ibm.com>
2018-01-24 18:17:42 +0900

    Add Header

---
+++ src/renderer/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+<v-toolbar app fixed clipped-left>
+  <div style="width:44px">
+    <v-toolbar-side-icon v-show="isLogin" @click.stop="showDrawer"></v-toolbar-side-icon>
+  </div>
+  <v-toolbar-title>Watson Conversation Dialog Tester</v-toolbar-title>
+</v-toolbar>
+</template>
+
+<script>
+import {
+  mapState,
+  mapMutations
+} from 'vuex'
+
+export default {
+  computed: {
+    ...mapState('Conversation', ['isLogin'])
+  },
+  methods: {
+    ...mapMutations('Config', ['showDrawer'])
+  }
+}
+</script>
